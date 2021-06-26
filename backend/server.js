d855bbfaf4338c0f249087a5b41739933fd8e4b1
Sai Prasad Dash <sabitadash007@gmail.com>
2021-06-26 10:38:39 +0530

    Mongodb setup and added user schema

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -17,8 +20,10 @@ app.get('/',(req,res)=>{
     res.send('API is Running');
 });
 
+// The port in which the app will run
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
+    // NODE_ENV can be development or in production mode
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold);
 });
